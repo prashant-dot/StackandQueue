bc1d0347e69f00b5d0ddf72c878bb9b4bfbef00f
prashant-dot <64984062+prashant-dot@users.noreply.github.com>
2021-06-23 12:10:12 +0530

    Amazon, Microsoft
    Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
    
    More formally,
    
        G[i] for an element A[i] = an element A[j] such that 
        j is maximum possible AND 
        j < i AND
        A[j] < A[i]
    Elements for which no smaller element exist, consider next smaller element as

---
+++ Nearest Smaller Element.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+	# @param A : list of integers
+	# @return a list of integers
+	def prevSmaller(self, A):
+        stack=deque()
+        l=[]
+        for i in range(len(A)):
+            x=-1
+            while(len(stack)!=0 and stack[-1]>=A[i]):
+                stack.pop()
+            if len(stack)!=0: x=stack[-1]
+            l.append(x)
+            stack.append(A[i])
+        return l       
+==>TC=O(n^2) and SC=O(n)
